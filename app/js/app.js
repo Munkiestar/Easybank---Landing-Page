743be213712491bdc1148cc54bbbfcaff0a83656
Igor <munkieryder@gmail.com>
2020-09-04 13:16:06 +0200

    mobile hamburger menu 'on click'

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,26 @@
 const hamburger = document.querySelector('.header-hamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeMenu = document.querySelectorAll('.has-fade');
 
 hamburger.addEventListener('click', () => {
   if (header.classList.contains('open')) {
+    //close hamburger menu
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    body.classList.remove('no-scroll');
+
+    fadeMenu.forEach((element) => {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
   } else {
+    //open hamburger menu
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    body.classList.add('no-scroll');
+    fadeMenu.forEach((element) => {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
   }
 });
